dfa3eef3050734b992ca2852d850491dcde73781
dutch <giacomo@vyperprotocol.io>
2022-09-17 18:36:19 +0200

    feat(footer improve): improved footer content and margin

--- components/templates/Footer/Footer.tsx
+++ components/templates/Footer/Footer.tsx
@@ -1,7 +1,12 @@
+import { Pane } from 'evergreen-ui';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-	return <footer className={styles.footer}>Copyright ©2022 - Built on Vyper Core</footer>;
+	return (
+		<footer className={styles.footer}>
+			<Pane margin={20}>Vyper OTC application built on Vyper Core | Copyright ©{new Date().getFullYear()}</Pane>
+		</footer>
+	);
 };
 
 export default Footer;
