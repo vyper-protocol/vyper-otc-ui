c1fdd54e093688d5b0785dc37433d073597c7085
pankaryp <x0pk@protonmail.com>
2022-09-28 18:11:15 +0300

    chore(selectwallet): conditionally render a custom text when wallet is connected

--- components/organisms/SelectWallet/SelectWallet.tsx
+++ components/organisms/SelectWallet/SelectWallet.tsx
@@ -1,9 +1,27 @@
+import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { ChevronDownIcon } from 'evergreen-ui';
 
+import { abbreviateAddress } from '../../../utils/stringHelpers';
 import styles from './SelectWallet.module.scss';
 
 const SelectWallet = () => {
-	return <WalletMultiButton className={styles.button} />;
+	const wallet = useWallet();
+
+	return !wallet.connected ? (
+		<WalletMultiButton className={styles.button} />
+	) : (
+		<WalletMultiButton className={styles.button}>
+			<div>
+				<p>
+					{abbreviateAddress(wallet.publicKey.toString())} <ChevronDownIcon />
+				</p>
+				<p>
+					{wallet.wallet.adapter.name} <span className={styles.active_badge}></span>
+				</p>
+			</div>
+		</WalletMultiButton>
+	);
 };
 
 export default SelectWallet;
