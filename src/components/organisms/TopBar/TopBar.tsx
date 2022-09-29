a50ed8484e4569158a5c4f8e924037702df91af9
Steven Hansel <stevenhanselgo@gmail.com>
2022-09-30 01:50:02 +0700

    feat(social-links): add social navigation tab
    49 - social links in top bar menu

--- src/components/organisms/TopBar/TopBar.tsx
+++ src/components/organisms/TopBar/TopBar.tsx
@@ -2,9 +2,22 @@
 import { useState } from 'react';
 
 import cn from 'classnames';
+import DiscordIcon from 'components/atoms/Icons/Discord';
+import TwitterIcon from 'components/atoms/Icons/Twitter';
 import SearchBar from 'components/molecules/SearchBar/SearchBar';
 import SelectWallet from 'components/organisms/SelectWallet/SelectWallet';
-import { Text, Pane, Heading, StackedChartIcon, CubeAddIcon, Tooltip } from 'evergreen-ui';
+import {
+	Text,
+	Pane,
+	Heading,
+	StackedChartIcon,
+	CubeAddIcon,
+	GridViewIcon,
+	ChevronDownIcon,
+	Tooltip,
+	Popover,
+	Position
+} from 'evergreen-ui';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -24,6 +37,19 @@ const menuItems = [
 		path: '/contract/create_contract',
 		wip: true,
 		tooltip: 'Coming soon'
+	}
+];
+
+const socialMediaItems = [
+	{
+		name: 'Twitter',
+		icon: <TwitterIcon />,
+		link: 'https://twitter.com/VyperProtocol'
+	},
+	{
+		name: 'Discord',
+		icon: <DiscordIcon />,
+		link: 'https://discord.com/invite/DuXVSgjmuW'
 	}
 ];
 
@@ -74,6 +100,37 @@ const TopBar = () => {
 							</div>
 						);
 					})}
+
+					<Popover
+						statelessProps={{
+							className: cn(styles.popover)
+						}}
+						position={Position.BOTTOM}
+						content={
+							<Pane className={styles.container}>
+								{socialMediaItems.map((item) => {
+									return (
+										<a
+											key={item.name}
+											className={styles.item}
+											href={item.link}
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											{item.icon}
+											<Text>{item.name}</Text>
+										</a>
+									);
+								})}
+							</Pane>
+						}
+					>
+						<div className={styles.item}>
+							<Text>
+								<GridViewIcon /> More <ChevronDownIcon />
+							</Text>
+						</div>
+					</Popover>
 				</Pane>
 
 				<SelectWallet />
