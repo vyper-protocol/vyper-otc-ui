371aa66efba838910327509aaf0968ce3c466877
Steven Hansel <stevenhanselgo@gmail.com>
2022-09-30 20:24:35 +0700

    feat(social-links): add more social links and adjust the styling
    #49

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -1,9 +1,14 @@
 import { IconBaseProps } from 'react-icons';
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
+import { FaDiscord, FaGithub, FaGlobe, FaMedium, FaTwitter } from 'react-icons/fa';
+import { MdArticle } from 'react-icons/md';
 
 const ICONS_MAP = {
 	FaDiscord: FaDiscord,
-	FaTwitter: FaTwitter
+	FaGithub: FaGithub,
+	FaGlobe: FaGlobe,
+	FaMedium: FaMedium,
+	FaTwitter: FaTwitter,
+	MdArticle: MdArticle
 };
 
 export type AvailableIconNames = keyof typeof ICONS_MAP;
