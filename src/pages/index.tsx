9b827a1cb4d3493028c1aacf655365d98217abdd
pankaryp <x0pk@protonmail.com>
2022-10-17 17:37:56 +0300

    chore(home): change home hero text, add telegram link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 
+import cn from 'classnames';
 import SearchBar from 'components/molecules/SearchBar';
 import TopBar from 'components/organisms/TopBar';
 import Footer from 'components/templates/Footer';
+import resources from 'configs/resources.json';
 import { Pane } from 'evergreen-ui';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -12,6 +14,10 @@ import styles from './index.module.scss';
 
 const Home = () => {
 	const [searchValue, setSearchValue] = useState('');
+
+	const telegram = resources.socialMedias.find((s) => {
+		return s.name === 'Telegram';
+	});
 
 	return (
 		<>
@@ -24,9 +30,21 @@ const Home = () => {
 					<SearchBar searchState={{ value: searchValue, setValue: setSearchValue }} className={styles.searchbar} />
 					<Image alt="abstract-colors" src={backgroundImage} layout="fill" objectFit="cover" quality={50} priority style={{ zIndex: -1 }} />
 				</div>
-				<div className={styles.text_wrapper}>
+				<div className={cn(styles.text_wrapper, styles.subtext)}>
+					<p>Wanna trade?</p>
+					<p>
+						Join our{' '}
+						<b>
+							<a target="_blank" href={telegram.link} rel="noopener noreferrer">
+								Telegram
+							</a>
+						</b>{' '}
+						group and request your trade
+					</p>
+				</div>
+				<div className={cn(styles.text_wrapper, styles.title)}>
 					<h2>VYPER OTC</h2>
-					<p className={styles.text}>The Derivatives Solana Platform</p>
+					<p>Trade the untradable with permissionless derivatives</p>
 				</div>
 				<Footer />
 			</Pane>
