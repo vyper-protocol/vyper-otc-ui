3623c0790de80e74341b9ba39a816c771ab35d2e
dutch <giacomo@vyperprotocol.io>
2022-10-18 16:21:51 +0200

    account change listener

--- src/components/organisms/actionButtons/DepositButton.tsx
+++ src/components/organisms/actionButtons/DepositButton.tsx
@@ -1,30 +1,74 @@
 /* eslint-disable no-console */
 import { useContext, useEffect, useState } from 'react';
 
-import { AnchorProvider } from '@project-serum/anchor';
+import { AnchorProvider, IdlAccounts, Program } from '@project-serum/anchor';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { PublicKey } from '@solana/web3.js';
 import { deposit } from 'api/otc-state/deposit';
 import ButtonPill from 'components/atoms/ButtonPill';
 import { TxHandlerContext } from 'components/providers/TxHandlerProvider';
 import { useGetFetchOTCStateQuery } from 'hooks/useGetFetchOTCStateQuery';
+import { VyperOtc, IDL as VyperOtcIDL } from 'idls/vyper_otc';
+import PROGRAMS from '../../../configs/programs.json';
+import { getAccount } from '@solana/spl-token';
 
 const DepositButton = ({ otcStatePubkey, isBuyer }: { otcStatePubkey: string; isBuyer: boolean }) => {
 	const { connection } = useConnection();
 	const wallet = useWallet();
 	const txHandler = useContext(TxHandlerContext);
+	const isSeller = !isBuyer;
 
 	const provider = new AnchorProvider(connection, wallet, {});
 	const rateStateQuery = useGetFetchOTCStateQuery(connection, otcStatePubkey);
 	const [isLoading, setIsLoading] = useState(false);
 
+	// flag for is available fetched in realtime via ws
+	const [isAvailable, setIsAvailable] = useState(true);
+
+	// listen for account changes
 	useEffect(() => {
-		if (rateStateQuery?.data?.buyerTA === null && rateStateQuery?.data?.sellerTA === null) {
-			rateStateQuery.refetch();
-		} else {
-			setIsLoading(false);
-		}
-	}, [rateStateQuery]);
+		const subscriptionId = connection.onAccountChange(
+			new PublicKey(otcStatePubkey),
+			async (updatedAccountInfo) => {
+				const vyperOtcProgram = new Program<VyperOtc>(VyperOtcIDL, new PublicKey(PROGRAMS.VYPER_OTC_PROGRAM_ID), new AnchorProvider(connection, undefined, {}));
+				const otcStateUpdate = vyperOtcProgram.coder.accounts.decode<IdlAccounts<VyperOtc>['otcState']>('otcState', updatedAccountInfo.data);
+
+				console.log('otcStateUpdate.seniorSideBeneficiary: ' + otcStateUpdate.seniorSideBeneficiary);
+				console.log('otcStateUpdate.juniorSideBeneficiary: ' + otcStateUpdate.juniorSideBeneficiary);
+
+				if (otcStateUpdate.seniorSideBeneficiary !== null) {
+					if (isBuyer) {
+						// buyer already take
+						setIsAvailable(false);
+					} else if (wallet?.publicKey) {
+						// settler not available if I'm the buyer
+						const seniorSideBeneficiary = await getAccount(connection, otcStateUpdate.seniorSideBeneficiary);
+						if (seniorSideBeneficiary.owner.equals(wallet.publicKey)) {
+							setIsAvailable(false);
+						}
+					}
+				}
+
+				if (otcStateUpdate.juniorSideBeneficiary !== null && isSeller) {
+					if (isSeller) {
+						// seller already take
+						setIsAvailable(false);
+					} else if (wallet?.publicKey) {
+						// buyer not available if I'm the seller
+						const juniorSideBeneficiary = await getAccount(connection, otcStateUpdate.juniorSideBeneficiary);
+						if (juniorSideBeneficiary.owner.equals(wallet.publicKey)) {
+							setIsAvailable(false);
+						}
+					}
+				}
+			},
+			'confirmed'
+		);
+
+		return () => {
+			connection.removeAccountChangeListener(subscriptionId);
+		};
+	});
 
 	const onDepositClick = async () => {
 		try {
@@ -34,6 +78,7 @@ const DepositButton = ({ otcStatePubkey, isBuyer }: { otcStatePubkey: string; is
 		} catch (err) {
 			console.log(err);
 		} finally {
+			setIsLoading(false);
 			rateStateQuery.refetch();
 		}
 	};
@@ -43,6 +88,10 @@ const DepositButton = ({ otcStatePubkey, isBuyer }: { otcStatePubkey: string; is
 			return <></>;
 		}
 	} else if (rateStateQuery?.data === undefined || !rateStateQuery?.data?.isDepositSellerAvailable(wallet.publicKey)) {
+		return <></>;
+	}
+
+	if (!isAvailable) {
 		return <></>;
 	}
 
