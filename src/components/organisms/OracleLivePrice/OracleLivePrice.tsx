c2d6eb7ded0a2876788fe91aedf870e508d6f0d6
dutch <giacomo@vyperprotocol.io>
2022-10-18 09:38:18 +0200

    fix init zero

--- src/components/organisms/OracleLivePrice/OracleLivePrice.tsx
+++ src/components/organisms/OracleLivePrice/OracleLivePrice.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 import { Skeleton } from '@mui/material';
 import { useConnection } from '@solana/wallet-adapter-react';
-import { PublicKey } from '@solana/web3.js';
+import { AccountInfo, Connection, PublicKey } from '@solana/web3.js';
 import { RatePluginTypeIds } from 'models/plugins/AbsPlugin';
 import { RatePythState } from 'models/plugins/rate/RatePythState';
 import RateSwitchboardState from 'models/plugins/rate/RateSwitchboardState';
@@ -19,23 +19,42 @@ const OracleLivePrice = ({ oracleType, pubkey }: OracleLivePriceInput) => {
 	const [priceValue, setPriceValue] = useState(0);
 	const [isInitialized, setIsInitialized] = useState(false);
 	const { connection } = useConnection();
+	const accountToWatch = new PublicKey(pubkey);
 
+	const decodeAccountInfo = async (updatedAccountInfo: AccountInfo<Buffer>): Promise<number> => {
+		let newPriceValue = 0;
+		if (oracleType === 'switchboard') {
+			newPriceValue = await RateSwitchboardState.DecodePriceFromAccountInfo(connection, updatedAccountInfo);
+		}
+		if (oracleType === 'pyth') {
+			newPriceValue = RatePythState.DecodePriceFromAccountInfo(updatedAccountInfo);
+		}
+		return newPriceValue;
+	};
+
+	// first fetch
 	useEffect(() => {
-		const accountToWatch = new PublicKey(pubkey);
+		const fetchData = async () => {
+			const accountData = await connection.getAccountInfo(accountToWatch);
+			const newPriceValue = await decodeAccountInfo(accountData);
+			setPriceValue(newPriceValue);
+			setIsInitialized(true);
+		};
+
+		fetchData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	// listen for account changes
+	useEffect(() => {
 		const subscriptionId = connection.onAccountChange(
 			accountToWatch,
 			async (updatedAccountInfo) => {
 				// console.log(updatedAccountInfo.data);
-				let newPriceValue = 0;
-				if (oracleType === 'switchboard') {
-					newPriceValue = await RateSwitchboardState.DecodePriceFromAccountInfo(connection, updatedAccountInfo);
-				}
-				if (oracleType === 'pyth') {
-					newPriceValue = RatePythState.DecodePriceFromAccountInfo(updatedAccountInfo);
-				}
+				const newPriceValue = await decodeAccountInfo(updatedAccountInfo);
 
 				// if price changed we update it
-				if (newPriceValue !== priceValue) {
+				if (newPriceValue !== priceValue || !isInitialized) {
 					console.log(`${oracleType} price changed for ${abbreviateAddress(pubkey)} from ${priceValue} to ${newPriceValue}`);
 					setPriceValue(newPriceValue);
 					setIsInitialized(true);
