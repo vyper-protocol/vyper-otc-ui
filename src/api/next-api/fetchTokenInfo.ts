a9a22c4193969fb62f6d7638353dc0a4ffe27d2d
dutch <giacomo@vyperprotocol.io>
2022-10-19 15:39:10 +0200

    feat(api token info): create new api endpoint for token info

---
+++ src/api/next-api/fetchTokenInfo.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+import { PublicKey } from '@solana/web3.js';
+import axios from 'axios';
+import { TokenInfo } from 'models/TokenInfo';
+
+export const fetchTokenInfo = async (mint: PublicKey): Promise<TokenInfo | undefined> => {
+	console.log('calling api endpoint, for mint: ' + mint);
+
+	const axiosRes = await axios.get('/api/token-info', {
+		params: {
+			mint: mint.toBase58()
+		}
+	});
+
+	return axiosRes.data;
+};
