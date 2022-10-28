ef9ba10aa7ab3d1954fd2f0f7adc27bcabab63a3
Shikhar <shikharshukla15606@gmail.com>
2022-10-28 12:50:47 +0530

    fix: hide the claim button after token transfer
    Added a listener to listen for token transfer and hide the claim button as soon as the transfer is
    completed.
    
    fix #249

--- src/components/organisms/actionButtons/ClaimButton.tsx
+++ src/components/organisms/actionButtons/ClaimButton.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable no-console */
-import { useContext, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 
-import { AnchorProvider } from '@project-serum/anchor';
+import { AnchorProvider, Program } from '@project-serum/anchor';
+import { unpackAccount } from '@solana/spl-token';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { PublicKey } from '@solana/web3.js';
 import { claim } from 'api/otc-state/claim';
 import ButtonPill from 'components/atoms/ButtonPill';
 import { TxHandlerContext } from 'components/providers/TxHandlerProvider';
 import { useGetFetchOTCStateQuery } from 'hooks/useGetFetchOTCStateQuery';
+import { VyperOtc, IDL as VyperOtcIDL } from 'idls/vyper_otc';
+
+import PROGRAMS from '../../../configs/programs.json';
 
 const ClaimButton = ({ otcStatePubkey, isBuyer }: { otcStatePubkey: string; isBuyer: boolean }) => {
 	const { connection } = useConnection();
@@ -17,6 +21,38 @@ const ClaimButton = ({ otcStatePubkey, isBuyer }: { otcStatePubkey: string; isBu
 	const provider = new AnchorProvider(connection, wallet, {});
 	const rateStateQuery = useGetFetchOTCStateQuery(connection, otcStatePubkey);
 	const [isLoading, setIsLoading] = useState(false);
+	const [isDeposited, setIsDeposited] = useState(false);
+
+	const getProgramTokenAccount = useCallback(async ()=>{
+		const vyperOtcProgram = new Program<VyperOtc>(VyperOtcIDL, new PublicKey(PROGRAMS.VYPER_OTC_PROGRAM_ID), new AnchorProvider(connection, undefined, {}));
+		const accountInfo = await vyperOtcProgram.account.otcState.fetch(otcStatePubkey);
+		if(isBuyer) {
+			return accountInfo.otcSeniorReserveTokenAccount;
+		}else{
+			return accountInfo.otcJuniorReserveTokenAccount;
+		}
+	}, [connection, isBuyer, otcStatePubkey]);
+
+	const tokenAccountListener = useCallback(async ()=>{
+		const programTokenAccount = await getProgramTokenAccount();
+		const subscriptionId = connection.onAccountChange(
+			programTokenAccount,
+			async (updatedAccountInfo) => {
+				const programTokenAmount = Number(unpackAccount(programTokenAccount, updatedAccountInfo).amount);
+				if(programTokenAmount === 0) setIsDeposited(true);
+			},
+			'confirmed'
+		);
+		return subscriptionId;
+	}, [connection, getProgramTokenAccount]);
+
+	useEffect(()=>{
+		let subscriptionId;
+		tokenAccountListener().then((id)=>{subscriptionId = id;});
+		return () => {
+			connection.removeAccountChangeListener(subscriptionId);
+		};
+	}, [connection, tokenAccountListener]);
 
 	const onClaimClick = async () => {
 		try {
@@ -30,8 +66,9 @@ const ClaimButton = ({ otcStatePubkey, isBuyer }: { otcStatePubkey: string; isBu
 			rateStateQuery.refetch();
 		}
 	};
-
-	if (isBuyer) {
+	if(isDeposited) {
+		return <></>;
+	} else if (isBuyer) {
 		if (rateStateQuery?.data === undefined || !rateStateQuery?.data?.isClaimSeniorAvailable(wallet.publicKey)) {
 			return <></>;
 		}
