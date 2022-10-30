637d843b8594892937be7036d5841d6dddd5cf62
dutch <giacomo@vyperprotocol.io>
2022-10-30 19:00:43 +0100

    included filter

--- src/api/supabase/selectContracts.ts
+++ src/api/supabase/selectContracts.ts
@@ -1,5 +1,6 @@
 import { FetchContractsParams } from 'controllers/fetchContracts/FetchContractsParams';
 import { DbOtcState } from 'models/DbOtcState';
+import { AVAILABLE_RATE_PLUGINS, AVAILABLE_REDEEM_LOGIC_PLUGINS } from 'models/plugins/AbsPlugin';
 
 import { CONTRACTS_METADATA_TABLE_NAME, CONTRACTS_TABLE_NAME, supabase } from './client';
 
@@ -18,6 +19,10 @@ export const selectContracts = async (params: FetchContractsParams): Promise<DbO
 	params.gt.forEach((f) => query.gt(f.column, f.value));
 	params.lt.forEach((f) => query.lt(f.column, f.value));
 	params.eq.forEach((f) => query.eq(f.column, f.value));
+
+	// filter for fetching only supported plugins in the UI
+	query.in('redeem_logic_plugin_type', AVAILABLE_REDEEM_LOGIC_PLUGINS);
+	query.in('rate_plugin_type', AVAILABLE_RATE_PLUGINS);
 
 	const res = await query;
 
