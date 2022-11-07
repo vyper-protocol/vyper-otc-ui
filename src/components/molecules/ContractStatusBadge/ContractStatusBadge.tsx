dc393bde869a76a13a94b4a4a14c26096ee845f5
Shikhar <shikharshukla15606@gmail.com>
2022-11-07 12:19:23 +0530

    refactor(components/molecules): mUI Migration for molecules
    refactored the components in molecules folder to use mui in place of evergreen ui
    
    re #263

--- src/components/molecules/ContractStatusBadge/ContractStatusBadge.tsx
+++ src/components/molecules/ContractStatusBadge/ContractStatusBadge.tsx
@@ -1,4 +1,4 @@
-import { Badge } from 'evergreen-ui';
+import { Badge } from '@mui/material';
 import { ContractStatusIds } from 'models/ChainOtcState';
 
 type ContractStatusBadgeInput = {
@@ -7,10 +7,10 @@ type ContractStatusBadgeInput = {
 
 const ContractStatusBadge = ({ status }: ContractStatusBadgeInput) => {
 	const getColorFromStatus = (s: ContractStatusIds) => {
-		if (s === 'active') return 'green';
-		if (s === 'expired') return 'red';
+		if (s === 'active') return 'success';
+		if (s === 'expired') return 'error';
 
-		return 'red';
+		return 'error';
 	};
 
 	const getLabelFromStatus = (s: ContractStatusIds) => {
@@ -21,9 +21,7 @@ const ContractStatusBadge = ({ status }: ContractStatusBadgeInput) => {
 	};
 
 	return (
-		<Badge color={getColorFromStatus(status)} margin={6}>
-			{getLabelFromStatus(status)}
-		</Badge>
+		<Badge badgeContent={getLabelFromStatus(status)} color={getColorFromStatus(status)} />
 	);
 };
 
