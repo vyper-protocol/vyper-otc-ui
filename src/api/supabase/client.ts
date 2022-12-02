04d9008cf186d9241c8c499ed61017cfa3131a07
dutch <giacomo@vyperprotocol.io>
2022-12-02 17:59:41 +0100

    dynamic data sync

--- src/api/supabase/client.ts
+++ src/api/supabase/client.ts
@@ -5,6 +5,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const CONTRACTS_TABLE_NAME = 'contracts';
 export const CONTRACTS_METADATA_TABLE_NAME = 'contracts_metadata';
+export const CONTRACTS_DYNAMIC_DATA_TABLE_NAME = 'contracts_dynamic_data';
 
 export const SNS_PUBLISHER_RPC_NAME = 'sns-publisher';
 
---
+++ src/api/supabase/syncContractData.ts
@@ -0,0 +1,68 @@
+/* eslint-disable no-console */
+/* eslint-disable camelcase */
+import { ChainOtcState } from 'models/ChainOtcState';
+
+import { CONTRACTS_DYNAMIC_DATA_TABLE_NAME, supabase } from './client';
+
+export const syncContractFromChain = async (otcState: ChainOtcState) => {
+	console.log('sync contract dynamic data');
+
+	let pricesAtSettlement1 = undefined;
+	let pricesAtSettlement2 = undefined;
+	let pnlBuyer = undefined;
+	let pnlSeller = undefined;
+
+	if (otcState.settleExecuted) {
+		try {
+			pricesAtSettlement1 = otcState.pricesAtSettlement[0];
+			if (otcState.pricesAtSettlement.length > 1) pricesAtSettlement2 = otcState.pricesAtSettlement[1];
+
+			pnlBuyer = otcState.getPnlBuyer(otcState.pricesAtSettlement);
+			pnlSeller = otcState.getPnlSeller(otcState.pricesAtSettlement);
+		} catch {}
+	}
+
+	const contractDynamicData = {
+		pubkey: otcState.publickey.toBase58(),
+
+		title: otcState.getContractTitle(),
+
+		buyer_ta: otcState.buyerTA?.toBase58(),
+		buyer_wallet: otcState.buyerWallet?.toBase58(),
+
+		seller_ta: otcState.sellerTA?.toBase58(),
+		seller_wallet: otcState.sellerWallet?.toBase58(),
+
+		settle_executed: otcState.settleExecuted,
+
+		prices_at_settlement_1: pricesAtSettlement1,
+		prices_at_settlement_2: pricesAtSettlement2,
+
+		pnl_buyer: pnlBuyer,
+		pnl_seller: pnlSeller
+	};
+
+	// check if entry exists
+	const { data: selectData, error: selectError } = await supabase
+		.from(CONTRACTS_DYNAMIC_DATA_TABLE_NAME)
+		.select('pubkey')
+		.eq('pubkey', otcState.publickey.toBase58());
+	if (selectError) {
+		console.error('error: ', selectError);
+		return;
+	}
+
+	if (selectData.length) {
+		console.log('update contract dynamic data for pubkey: ' + otcState.publickey.toBase58());
+		const { error } = await supabase.from(CONTRACTS_DYNAMIC_DATA_TABLE_NAME).update([contractDynamicData]).eq('pubkey', otcState.publickey.toBase58());
+		if (error) {
+			console.error('error: ', error);
+		}
+	} else {
+		console.log('insert new contract dynamic data for pubkey: ' + otcState.publickey.toBase58());
+		const { error } = await supabase.from(CONTRACTS_DYNAMIC_DATA_TABLE_NAME).insert([contractDynamicData]);
+		if (error) {
+			console.error('error: ', error);
+		}
+	}
+};
