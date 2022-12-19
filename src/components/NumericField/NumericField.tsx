a85058ed96da4f742a1d786b75795bbcc8958f32
giacomo <thegreat.vanderlinde@gmail.com>
2022-12-19 17:48:50 +0100

    removed console error for turkish kebab

--- src/components/NumericField/NumericField.tsx
+++ src/components/NumericField/NumericField.tsx
@@ -13,14 +13,14 @@ const NumericField = ({ label, value, onChange }: NumericFieldProps) => {
 				alignItems: 'center',
 				marginX: 2,
 				'& input[type=number]': {
-					'-moz-appearance': 'textfield'
+					MozAppearance: 'textfield'
 				},
 				'& input[type=number]::-webkit-outer-spin-button': {
-					'-webkit-appearance': 'none',
+					WebkitAppearance: 'none',
 					margin: 0
 				},
 				'& input[type=number]::-webkit-inner-spin-button': {
-					'-webkit-appearance': 'none',
+					WebkitAppearance: 'none',
 					margin: 0
 				}
 			}}
