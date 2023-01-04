e38d25fd2f8c0fae0bdea7d0760be66cf4498d22
vantessel <lollo9966@gmail.com>
2023-01-04 11:49:08 +0100

    add bonk down

--- src/pages/featured/bonk-up/index.tsx
+++ src/pages/featured/bonk-up/index.tsx
@@ -3,7 +3,7 @@ import { getCurrentCluster } from 'components/providers/OtcConnectionProvider';
 import Custom404Page from 'pages/404';
 
 const BonkPage = () => {
-	return getCurrentCluster() === 'devnet' ? <Custom404Page /> : <BonkFixedPayout />;
+	return getCurrentCluster() === 'devnet' ? <Custom404Page /> : <BonkFixedPayout pageTitle="BONK UP" isCall={true} strike={1.03} />;
 };
 
 export default BonkPage;
