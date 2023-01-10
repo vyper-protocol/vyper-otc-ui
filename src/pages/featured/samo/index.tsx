174778808469835be380e041065d8d3bebb9c9e0
Federico <federico@vyperprotocol.io>
2023-01-10 13:15:24 -0400

    new component and logo

---
+++ src/pages/featured/samo/index.tsx
@@ -0,0 +1,16 @@
+import { getCurrentCluster } from 'components/providers/OtcConnectionProvider';
+import SamoFixedPayout from 'components/SamoFixedPayout';
+import Layout from 'components/templates/Layout';
+import Custom404Page from 'pages/404';
+
+const BonkPage = () => {
+	return getCurrentCluster() === 'devnet' ? (
+		<Custom404Page />
+	) : (
+		<Layout pageTitle="SAMO!">
+			<SamoFixedPayout />
+		</Layout>
+	);
+};
+
+export default BonkPage;
