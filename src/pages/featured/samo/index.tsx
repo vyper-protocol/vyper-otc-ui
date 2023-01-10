3a076c46f36cbd90929f7a43827e124a7a5fb90a
Federico <federico@vyperprotocol.io>
2023-01-10 13:17:02 -0400

    typo

--- src/pages/featured/samo/index.tsx
+++ src/pages/featured/samo/index.tsx
@@ -3,7 +3,7 @@ import SamoFixedPayout from 'components/SamoFixedPayout';
 import Layout from 'components/templates/Layout';
 import Custom404Page from 'pages/404';
 
-const BonkPage = () => {
+const SamoPage = () => {
 	return getCurrentCluster() === 'devnet' ? (
 		<Custom404Page />
 	) : (
@@ -13,4 +13,4 @@ const BonkPage = () => {
 	);
 };
 
-export default BonkPage;
+export default SamoPage;
